e530c21310ec1440d5225257abe8aea0f10dd273
toopeachok <2peachok@gmail.com>
2021-01-08 22:54:46 +0300

    Refactor Variable Names

--- src/MandelbrotPainter.java
+++ src/MandelbrotPainter.java
@@ -13,16 +13,16 @@ public class MandelbrotPainter {
   public static void main(String[] args) throws Exception {
     CartesianPlane cartesianPlane = new CartesianPlane(2, -2, 2, -2);
 
-    final int width = 640, height = 640;
+    final int WIDTH = 640, HEIGHT = 640;
 
-    ViewportPlane viewportPlane = new ViewportPlane(width, height);
+    ViewportPlane viewportPlane = new ViewportPlane(WIDTH, HEIGHT);
 
-    BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+    BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
 
     final int black = 0x000000, white = 0xFFFFFF;
 
-    for (int row = 0; row < height; row++) {
-      for (int col = 0; col < width; col++) {
+    for (int row = 0; row < HEIGHT; row++) {
+      for (int col = 0; col < WIDTH; col++) {
         Point viewportPoint = new Point(col, row);
         Point cartesianPoint = CoordinatesConverter.viewportToCartesian(viewportPoint, cartesianPlane, viewportPlane);
         float c_re = cartesianPoint.x;
--- src/utils/CartesianPlane.java
+++ src/utils/CartesianPlane.java
@@ -1,14 +1,14 @@
 package utils;
 
 public class CartesianPlane {
-  public final float xMin;
   public final float xMax;
-  public final float yMin;
+  public final float xMin;
   public final float yMax;
+  public final float yMin;
 
   public CartesianPlane(float xMax, float xMin, float yMax, float yMin) {
-    this.xMin = xMin;
     this.xMax = xMax;
+    this.xMin = xMin;
     this.yMax = yMax;
     this.yMin = yMin;
   }
