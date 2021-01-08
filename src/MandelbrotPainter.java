10ba53d9ed47d22b316c690c1fbece41dfd1208a
toopeachok <2peachok@gmail.com>
2021-01-09 00:08:56 +0300

    Draw Mandelbrot Set With Swing Components

--- src/MandelbrotPainter.java
+++ src/MandelbrotPainter.java
@@ -5,39 +5,70 @@ import utils.CoordinatesConverter;
 import utils.Point;
 import utils.ViewportPlane;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import javax.imageio.ImageIO;
+import javax.swing.*;
 
-public class MandelbrotPainter {
-  public static void main(String[] args) throws Exception {
-    CartesianPlane cartesianPlane = new CartesianPlane(2, -2, 2, -2);
+public class MandelbrotPainter extends JFrame {
+  private static final int WIDTH = 640, HEIGHT = 640;
 
-    final int WIDTH = 640, HEIGHT = 640;
+  public MandelbrotPainter() {
+    super("Mandelbrot Set");
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setSize(WIDTH, HEIGHT);
+    add(new Canvas());
+    setVisible(true);
+  }
 
-    ViewportPlane viewportPlane = new ViewportPlane(WIDTH, HEIGHT);
+  public static void main(String[] args) {
+    SwingUtilities.invokeLater(
+      new Runnable() {
+        @Override
+        public void run() {
+          new MandelbrotPainter();
+        }
+      }
+    );
+  }
+
+  static class Canvas extends JPanel {
+    private static final int WIDTH = 640, HEIGHT = 640;
+    private static final int black = 0x000000, white = 0xFFFFFF;
 
     BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
 
-    final int black = 0x000000, white = 0xFFFFFF;
+    private final CartesianPlane cartesianPlane = new CartesianPlane(2, -2, 2, -2);
+    private final ViewportPlane viewportPlane = new ViewportPlane(WIDTH, HEIGHT);
 
-    for (int row = 0; row < HEIGHT; row++) {
-      for (int col = 0; col < WIDTH; col++) {
-        Point viewportPoint = new Point(col, row);
-        Point cartesianPoint = CoordinatesConverter.viewportToCartesian(viewportPoint, cartesianPlane, viewportPlane);
-        float c_re = cartesianPoint.x;
-        float c_im = cartesianPoint.y;
-        ComplexNumber c = new ComplexNumber(c_re, c_im);
-
-        if (MandelbrotSet.isInSet(c)) {
-          image.setRGB(col, row, black);
-        } else {
-          image.setRGB(col, row, white);
-        }
-
-      }
+    public Canvas() {
+      super(true);
+      setSize(WIDTH, HEIGHT);
+      render();
     }
 
-    ImageIO.write(image, "png", new File("mandelbrot.png"));
+    @Override
+    public void paint(Graphics g) {
+      g.drawImage(image, 0, 0, null);
+    }
+
+    public void render() {
+      for (int row = 0; row < HEIGHT; row++) {
+        for (int col = 0; col < WIDTH; col++) {
+          final Point viewportPoint = new Point(col, row);
+          final Point cartesianPoint = CoordinatesConverter.viewportToCartesian(viewportPoint, cartesianPlane, viewportPlane);
+          final float c_re = cartesianPoint.x;
+          final float c_im = cartesianPoint.y;
+          final ComplexNumber c = new ComplexNumber(c_re, c_im);
+
+          if (MandelbrotSet.isInSet(c)) {
+            image.setRGB(col, row, black);
+          } else {
+            image.setRGB(col, row, white);
+          }
+
+        }
+      }
+    }
   }
+
 }
